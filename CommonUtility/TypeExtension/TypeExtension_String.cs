96e91af50d9390cd4625bc6b4743840862ec9c30
KenWoo <KenWoo2014@outlook.com>
2016-10-19 10:52:33 +0800

    分类0

--- CommonUtility/TypeExtension/TypeExtension_String.cs
+++ CommonUtility/TypeExtension/TypeExtension_String.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CommonUtility
+namespace CommonUtility.TypeExtension
 {
 
     public static partial class TypeExtension_String
